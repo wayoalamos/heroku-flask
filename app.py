9798fb8c27332c1b434847f39b56d78d59a25848
wayoalamos <malamos2@uc.cl>
2018-12-18 15:28:25 -0300

    add function

--- app.py
+++ app.py
@@ -8,10 +8,24 @@ def homepage():
 
     return """
     <h1>Hello heroku</h1>
+    <a href="/getPlotCSV">Click me.</a><a href
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+@app.route("/getPlotCSV")
+def getPlotCSV():
+    # with open("outputs/Adjacency.csv") as fp:
+    #     csv = fp.read()
+    csv = '1,2,3\n4,5,6\n'
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=portalDs.csv"})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
