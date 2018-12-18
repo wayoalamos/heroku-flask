cf7d59eee0d58dbd1b6fcb4d5bc8f2f641577502
wayoalamos <malamos2@uc.cl>
2018-12-18 15:24:22 -0300

    change 2

--- app.py
+++ app.py
@@ -5,20 +5,8 @@ app = Flask(__name__)
 def hello():
     return '''
         <html><body>
-        Hellssssso. <a href="/getPlotCSV">Click me.</a>
+        Hellssssso.
         </body></html>
         '''
-
-@app.route("/getPlotCSV")
-def getPlotCSV():
-    # with open("outputs/Adjacency.csv") as fp:
-    #     csv = fp.read()
-    csv = '1,2,3\n4,5,6\n'
-    return Response(
-        csv,
-        mimetype="text/csv",
-        headers={"Content-disposition":
-                 "attachment; filename=myplot.csv"})
-
 
 app.run(debug=True)
