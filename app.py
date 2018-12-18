b956978adf531c73f3ceb584e7241d251c6fb1f9
wayoalamos <malamos2@uc.cl>
2018-12-18 15:26:13 -0300

    change

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, Response
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return '''
-        <html><body>
-        Hellssssso.
-        </body></html>
-        '''
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-app.run(debug=True)
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
